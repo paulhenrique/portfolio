f66ef3fb6161d6246647e97fd5e78ab5fa974e25
Paulo Henrique <phvcandido@gmail.com>
2021-05-28 20:58:30 -0300

    Adicionando variação de fonte

--- src/pages/Bio/index.tsx
+++ src/pages/Bio/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 function Bio() {
   return (
-    <div>
-      <h1>Bio</h1>
+    <div className="bio">
+      <div className="card">
+        <div className="cardContainer">
+          <img src="/img/logo.png" alt="Logo PH" />
+          <div>
+            <h1>Paulo <br></br>Henrique</h1>
+            <p>Apaixonado por desenvolver e construir sistemas, com o título de técnico em informática pelo IFSP Campus Itapetininga participei de diferentes projetos com os quais aprendi muito, recebi reconhecimento através de menções honrosas na cidade de Itapetininga, tendo trabalhado junto da prefeitura e em projetos locais na cidade, sempre desenvolvendo identidades visuais e aprimorando interfaces. Trabalhei no desenvolvimento de diferentes sistemas, tendo hoje a certificação Harvard CS50, desenvolvi recursos em Python e Javascript. </p>
+          </div>
+          <Link className="portfolio" to="/portfolio">Ver Portfolio</Link>
+        </div>
+      </div>
     </div>
   )
 }
