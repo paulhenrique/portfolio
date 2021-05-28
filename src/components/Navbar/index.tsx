0e0ac7c18aa2354fc2f46bf3038db0582f232d72
Paulo Henrique <phvcandido@gmail.com>
2021-05-28 20:59:59 -0300

    Finalizando desktop da bio

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,9 +6,9 @@ function Navbar() {
   return (
     <div className={styles.navbar}>
       <NavLink activeClassName={styles.activeClass} exact to="/">Bio</NavLink>
-      <NavLink activeClassName={styles.activeClass} to="/portfolio" >Portfólio</NavLink>
-      <NavLink activeClassName={styles.activeClass} to="/experiencia" >Experiência</NavLink>
-      <NavLink activeClassName={styles.activeClass} to="/contato" >Contato</NavLink>
+      <NavLink activeClassName={styles.activeClass} to="/portfolio" >Portfolio</NavLink>
+      <NavLink activeClassName={styles.activeClass} to="/experiencia" >Experience</NavLink>
+      <NavLink activeClassName={styles.activeClass} to="/contato" >Contact</NavLink>
     </div>
   )
 }
