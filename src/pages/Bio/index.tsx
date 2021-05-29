78bb7b3322f54c405a725501e46fa19a9e435ec3
Paulo Henrique <phvcandido@gmail.com>
2021-05-28 23:46:57 -0300

    Atualizando Bio

--- src/pages/Bio/index.tsx
+++ src/pages/Bio/index.tsx
@@ -12,8 +12,7 @@ function Bio() {
             <h1>Paulo <br></br>Henrique</h1>
             <p>Apaixonado por desenvolver e construir sistemas, com o título de técnico em informática pelo IFSP Campus Itapetininga participei de diferentes projetos com os quais aprendi muito, recebi reconhecimento através de menções honrosas na cidade de Itapetininga, tendo trabalhado junto da prefeitura e em projetos locais na cidade, sempre desenvolvendo identidades visuais e aprimorando interfaces. Trabalhei no desenvolvimento de diferentes sistemas, tendo hoje a certificação Harvard CS50, desenvolvi recursos em Python e Javascript. </p>
           </div>
-          <Link className="portfolio" to="/portfolio">Ver Portfolio
-          <i className="fas fa-arrow-right"></i>
+          <Link className="portfolio" to="/portfolio">Ver Portfolio    <i className="fas fa-arrow-right"></i>
           </Link>
         </div>
       </div>
